9badbf9ded31fe583ef44a0a8a27fc470d335833
21tnguyen <21tnguyen10@gmail.com>
2025-03-05 13:49:51 -0800

    creating flow to reset password
    - based on email address
    - user enters a reset code and a new password
    - updates user's password
    
    Co-Authored-By: Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>

--- app/(auth)/password-change.tsx
+++ app/(auth)/password-change.tsx
@@ -1,8 +1,8 @@
 import { useSignIn } from '@clerk/clerk-expo'
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 import { Link, useRouter } from "expo-router";
 import * as React from 'react';
-import { Text, TextInput, Button, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TextInput, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { AppColors } from '@/constants/Colors';
@@ -12,21 +12,58 @@ export default function ForgotPassword() {
     const { isLoaded, signIn, setActive } = useSignIn();
     const router = useRouter();
 
-    const [emailAddress, setEmailAddress] = React.useState('');
-    const [status, setStatus] = React.useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('')
+    const [code, setCode] = useState('');
+    const [status, setStatus] = useState('');
+    const [successfulCreation, setSuccessfulCreation] = useState(false);
+    const [error, setError] = useState('');
+
+    // useEffect(() => {
+    //     if (!isLoaded) return;
+    // }, [isLoaded]);
 
     const onRecoverPasswordPress = async () => {
-        if (!isLoaded) return;
+        if (!isLoaded || !signIn) return;
 
         try {
             const passwordRecovery = await signIn.create({
                 identifier: emailAddress.trim(),
                 strategy: "reset_password_email_code",
             });
+            setSuccessfulCreation(true);
             setStatus('Password reset link sent');
+            setError('');
             console.log("Reset link sent successfully");
-        } catch (err) {
+        } catch (err: any) {
             setStatus('Error sending reset link');
+            setError(err.errors?.[0]?.longMessage || 'An error occurred, Please try again');
+            console.log("Reset link unsuccessful");
+            console.error(JSON.stringify(err, null, 2));
+        }
+    };
+
+    const onResetPasswordPress = async () => {
+        if (!isLoaded || !signIn) return;
+
+        try {
+            const reset = await signIn.attemptFirstFactor({
+                strategy: "reset_password_email_code",
+                code,
+                password,
+            });
+
+            if (reset.status == 'complete' && reset.createdSessionId) {
+                setActive({ session: reset.createdSessionId });
+                setError('');
+                router.push('/')
+            } else {
+                setStatus('Unable to reset password');
+            }
+
+        } catch (err: any) {
+            setStatus('Error resetting password');
+            setError(err.errors?.[0]?.longMessage || 'An error occurred. Please try again');
             console.log("Reset link unsuccessful");
             console.error(JSON.stringify(err, null, 2));
         }
@@ -42,34 +79,77 @@ export default function ForgotPassword() {
                     />
                 <ThemedText style={{alignSelf: 'center', fontSize: 20}}>Forgot your password?</ThemedText>
 
-                <LinearGradient
-                    colors={[AppColors.LightBlue, AppColors.White]}
-                    style={styles.input}
-                    start={{ x:0, y: 0}}
-                    end={{ x: 1, y: 0}}
-                >
-                
-                    <TextInput
-                        style={{ "color": 'black', marginLeft: 10 }}
-                        autoCapitalize='none'
-                        value={emailAddress}
-                        placeholder="Email"
-                        placeholderTextColor="#666666"
-                        onChangeText={setEmailAddress}
-                    />
-                </LinearGradient>
+                {!successfulCreation ? (
+                    <>
+                        <LinearGradient
+                        colors={[AppColors.LightBlue, AppColors.White]}
+                        style={styles.input}
+                        start={{ x:0, y: 0}}
+                        end={{ x: 1, y: 0}}
+                        >
+                    
+                            <TextInput
+                                style={{ "color": 'black', marginLeft: 10 }}
+                                autoCapitalize='none'
+                                value={emailAddress}
+                                placeholder="Email"
+                                placeholderTextColor="#666666"
+                                onChangeText={setEmailAddress}
+                            />
+                        </LinearGradient>
 
-                <View style={{ alignItems: 'center', marginTop: 2 }}>
-                    <LinearGradient
-                        colors={[AppColors.Purple, AppColors.LightBlue]}
-                        style={styles.buttonRecoverPassword}
-                    >
-                        <TouchableOpacity onPress={onRecoverPasswordPress}>
-                            <ThemedText style={styles.buttonText}>Recover password</ThemedText>
+                        <View style={{ alignItems: 'center', marginTop: 2 }}>
+                            <TouchableOpacity onPress={onRecoverPasswordPress}>
+                                <LinearGradient
+                                    colors={[AppColors.Purple, AppColors.LightBlue]}
+                                    style={styles.buttonRecoverPassword}
+                                >
+                                    <ThemedText style={styles.buttonText}>Recover password</ThemedText>
+                                </LinearGradient> 
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                ):(
+                    <>
+                        <LinearGradient
+                            colors={[AppColors.LightBlue, AppColors.White]}
+                            style={styles.input}
+                            start={{ x:0, y: 0}}
+                            end={{ x: 1, y: 0}}
+                        >
+                            <TextInput
+                                style={{ "color": 'black', marginLeft: 10 }}
+                                autoCapitalize='none'
+                                secureTextEntry
+                                value={password}
+                                placeholder="New Password"
+                                placeholderTextColor="#666666"
+                                onChangeText={setPassword}
+                            />
+                        </LinearGradient>
+
+                        <TouchableOpacity onPress={onResetPasswordPress} style={styles.buttonRecoverPassword}>
+                            <LinearGradient
+                                colors={[AppColors.LightBlue, AppColors.White]}
+                                style={styles.input}
+                                start={{ x:0, y: 0}}
+                                end={{ x: 1, y: 0}}
+                                >
+                                <TextInput
+                                    style={{ "color": 'black', marginLeft: 10 }}
+                                    autoCapitalize='none'
+                                    value={code}
+                                    placeholder="Reset Password Code"
+                                    placeholderTextColor="#666666"
+                                    onChangeText={setCode}
+                                />
+                            </LinearGradient>
+                            <ThemedText style={styles.buttonText}>Reset password</ThemedText>
                         </TouchableOpacity>
-                    </LinearGradient> 
-                </View>
+                    </>
+                )}
 
+                {error && <Text style={{ "color": 'red', textAlign: 'center', marginTop: 10 }}>{error}</Text>}
 
                 </ThemedView>
                     <View style={styles.bottomView}>
@@ -83,38 +163,38 @@ export default function ForgotPassword() {
                     </View>
             </>
         </ThemedView>
-    )
+    );
 }
 
-        const styles = StyleSheet.create({
-            buttonText: {
-                fontWeight: 'bold',
-            },
+const styles = StyleSheet.create({
+    buttonText: {
+        fontWeight: 'bold',
+    },
 
-            buttonRecoverPassword: {
-                padding: 10,
-                marginTop: 10,
-                alignItems: "center",
-                justifyContent: "center",
-                borderRadius: 25,
-                width: "60%",
-            },
+    buttonRecoverPassword: {
+        padding: 10,
+        marginTop: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 25,
+        width: "60%",
+    },
 
-            bottomView: {
-                backgroundColor: 'white',
-                alignSelf: 'center',
-                marginTop: 20,
-            },
+    bottomView: {
+        backgroundColor: 'white',
+        alignSelf: 'center',
+        marginTop: 20,
+    },
 
-            input: {
-                borderRadius: 25,
-                marginTop: 10,
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.25,
-                shadowRadius: 3.84,
-                elevation: 5,
-                padding: 15,
-            },
+    input: {
+        borderRadius: 25,
+        marginTop: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        padding: 15,
+    },
 
 });
