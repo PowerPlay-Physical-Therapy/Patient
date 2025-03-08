01186d67fe5650afc915a08116ec962a43fff756
21tnguyen <21tnguyen10@gmail.com>
2025-03-08 11:52:33 -0800

    finished password change
    - fixed formatting issues
    - prompted user to sign in with their new password
    
    Co-Authored-By: Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>

--- app/(auth)/password-change.tsx
+++ app/(auth)/password-change.tsx
@@ -1,4 +1,4 @@
-import { useSignIn } from '@clerk/clerk-expo'
+import { useSignIn, useAuth } from '@clerk/clerk-expo'
 import { useEffect, useState } from 'react';
 import { Link, useRouter } from "expo-router";
 import * as React from 'react';
@@ -7,6 +7,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { AppColors } from '@/constants/Colors';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useUser } from '@clerk/clerk-expo';
 
 export default function ForgotPassword() {
     const { isLoaded, signIn, setActive } = useSignIn();
@@ -19,16 +20,16 @@ export default function ForgotPassword() {
     const [successfulCreation, setSuccessfulCreation] = useState(false);
     const [error, setError] = useState('');
 
-    // useEffect(() => {
-    //     if (!isLoaded) return;
-    // }, [isLoaded]);
+    const { signOut } = useAuth();
 
+    // Send password recovery email when 'Recover passowrd' is pressed
     const onRecoverPasswordPress = async () => {
         if (!isLoaded || !signIn) return;
 
         try {
             const passwordRecovery = await signIn.create({
                 identifier: emailAddress.trim(),
+                // Send reset code to email
                 strategy: "reset_password_email_code",
             });
             setSuccessfulCreation(true);
@@ -39,24 +40,36 @@ export default function ForgotPassword() {
             setStatus('Error sending reset link');
             setError(err.errors?.[0]?.longMessage || 'An error occurred, Please try again');
             console.log("Reset link unsuccessful");
+            // See https://clerk.com/docs/custom-flows/error-handling
+            // for more info on error handling
             console.error(JSON.stringify(err, null, 2));
         }
     };
 
+    // Once the new password and correct code is inputted, password is reset when 'Reset password' is pressed
     const onResetPasswordPress = async () => {
-        if (!isLoaded || !signIn) return;
+        if (!isLoaded || !signIn) {
+            setStatus("Authentication not loaded. Please try again");
+            return;
+        }
 
         try {
+            // Attempts to reset the password with the new password and code
             const reset = await signIn.attemptFirstFactor({
                 strategy: "reset_password_email_code",
                 code,
                 password,
             });
 
-            if (reset.status == 'complete' && reset.createdSessionId) {
-                setActive({ session: reset.createdSessionId });
+            // After the password reset is successful, the User signs in with their new password
+            if (reset?.status === 'complete') {
+                setStatus("Password reset successful. Please sign in again");
                 setError('');
-                router.push('/')
+                
+                // Sign the User out to have them sign in with their new password
+                await signOut();
+                router.replace('/sign-in')
+                return;
             } else {
                 setStatus('Unable to reset password');
             }
@@ -65,20 +78,29 @@ export default function ForgotPassword() {
             setStatus('Error resetting password');
             setError(err.errors?.[0]?.longMessage || 'An error occurred. Please try again');
             console.log("Reset link unsuccessful");
+            // See https://clerk.com/docs/custom-flows/error-handling
+            // for more info on error handling
             console.error(JSON.stringify(err, null, 2));
+            return;
         }
     };
+        
 
     return (
         <ThemedView style={{ flex: 1, justifyContent: 'center', padding: 40 }}>
             <>
+            {/* PowerPlay Logo */}
             <ThemedView style= {{ flex: 1, justifyContent: 'center' }}>
                     <Image
                         source={require('@/assets/images/app-logo.png')}
                         style={{ width: 140, height: 140, alignSelf: 'center', marginBottom: 20 }}
                     />
-                <ThemedText style={{alignSelf: 'center', fontSize: 20}}>Forgot your password?</ThemedText>
+                {/* Text on 'Forgot your password' page */}
+                <ThemedText style={{alignSelf: 'center', fontSize: 20}}>
+                    Forgot your password?
+                </ThemedText>
 
+                {/* If password reset link has not been sent */}
                 {!successfulCreation ? (
                     <>
                         <LinearGradient
@@ -98,11 +120,12 @@ export default function ForgotPassword() {
                             />
                         </LinearGradient>
 
+                        {/* Recover password button */}
                         <View style={{ alignItems: 'center', marginTop: 2 }}>
                             <TouchableOpacity onPress={onRecoverPasswordPress}>
                                 <LinearGradient
                                     colors={[AppColors.Purple, AppColors.LightBlue]}
-                                    style={styles.buttonRecoverPassword}
+                                    style={styles.button}
                                 >
                                     <ThemedText style={styles.buttonText}>Recover password</ThemedText>
                                 </LinearGradient> 
@@ -111,6 +134,7 @@ export default function ForgotPassword() {
                     </>
                 ):(
                     <>
+                        {/* If password reset link has been sent, User inputs new password*/}
                         <LinearGradient
                             colors={[AppColors.LightBlue, AppColors.White]}
                             style={styles.input}
@@ -128,8 +152,8 @@ export default function ForgotPassword() {
                             />
                         </LinearGradient>
 
-                        <TouchableOpacity onPress={onResetPasswordPress} style={styles.buttonRecoverPassword}>
-                            <LinearGradient
+                        {/* Verification code input */}
+                        <LinearGradient
                                 colors={[AppColors.LightBlue, AppColors.White]}
                                 style={styles.input}
                                 start={{ x:0, y: 0}}
@@ -139,18 +163,29 @@ export default function ForgotPassword() {
                                     style={{ "color": 'black', marginLeft: 10 }}
                                     autoCapitalize='none'
                                     value={code}
-                                    placeholder="Reset Password Code"
+                                    placeholder="Code"
                                     placeholderTextColor="#666666"
                                     onChangeText={setCode}
                                 />
                             </LinearGradient>
-                            <ThemedText style={styles.buttonText}>Reset password</ThemedText>
-                        </TouchableOpacity>
+                        
+                        {/* Reset button */}
+                        <View style={{ alignItems: 'center', marginTop: 2 }}>
+                            <TouchableOpacity onPress={onResetPasswordPress}>
+                                <LinearGradient
+                                    colors={[AppColors.Purple, AppColors.LightBlue]}
+                                    style={styles.button}
+                                >
+                                    <ThemedText style={styles.buttonText}>Reset password</ThemedText>
+                                </LinearGradient> 
+                            </TouchableOpacity>
+                        </View>
                     </>
                 )}
 
                 {error && <Text style={{ "color": 'red', textAlign: 'center', marginTop: 10 }}>{error}</Text>}
 
+                {/* Link to sign-in page */}
                 </ThemedView>
                     <View style={styles.bottomView}>
                         <ThemedText>
@@ -171,8 +206,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
 
-    buttonRecoverPassword: {
-        padding: 10,
+    button: {
+        padding: 13,
         marginTop: 10,
         alignItems: "center",
         justifyContent: "center",
