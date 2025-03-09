fd08dac184997f6d44ea3e9f5c0388ea9352382c
Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>
2025-03-08 20:14:56 -0800

    Explore page Basic UI

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, ScrollView, View, Dimensions } from 'react-native';
 import { AppColors } from '@/constants/Colors';
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -7,11 +7,47 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { LinearGradient } from 'expo-linear-gradient';
 import ScreenHeader from '@/components/ScreenHeader';
+import { useEffect, useState } from 'react';
+import { Link } from 'expo-router';
+import { rgbaColor } from 'react-native-reanimated/lib/typescript/Colors';
+import { setStatusBarTranslucent } from 'expo-status-bar';
+
+const {height, width} = Dimensions.get("window")
 
 export default function ExploreScreen() {
+  // const [exploreAll, setExploreAll] = useState([]) 
+  
+  const exploreAll = require('@/assets/Exercises.json');
+  // useEffect(() => {
+  //   fetch('@/assets/Exercises.json')
+  //     .then(data => setExploreAll(data));
+  // }, []);
+
   return (
       <LinearGradient style={{ flex: 1, paddingTop: Platform.OS == 'ios' ? 50 : 0}} colors={[AppColors.OffWhite, AppColors.LightBlue]}>
         <ScreenHeader title="Explore" />
+
+        <ScrollView>
+              {exploreAll.map((category) => (
+                <View style={{ padding: 16 }}>
+                  <ThemedText>{category.title}</ThemedText>
+                  {category["subcategory"].map((subcategory) => (
+                    <View style={{ margin: 5, padding:5, backgroundColor : AppColors.OffWhite , borderRadius: 15}}>
+                      <ThemedText style={{paddingLeft:5}}>{subcategory.subtitle}</ThemedText>
+                      <ScrollView horizontal={true}>
+                        {subcategory["exercises"].map((exercise) => (
+                          <View style={{ backgroundColor: AppColors.Green, alignItems: "center", justifyContent: "flex-end", margin: 5, borderRadius: 15, zIndex: 0, shadowOffset: { height: 2, width: 2 }, shadowRadius: 16, shadowOpacity: 0.5 }}>
+                            <Image source={{ uri: exercise.thumbnail_url }} style={{ width: width * 0.5, height: height * 0.2, borderRadius: 15, zIndex: 2 }}/>
+                            <ThemedText style={{ position: "absolute" , zIndex:3 , backgroundColor: 'rgba(0,0,0,0.5)', borderRadius: 5, padding:2}} >{exercise.name}</ThemedText>
+                          </View>
+                        ))}
+                      </ScrollView>
+                    </View>
+                  ))}
+                </View>
+              ))}
+        </ScrollView>
+
       </LinearGradient>
   )
 }
@@ -21,4 +57,14 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
       height: '100%',
-  } });
+  },
+  thumbnail: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  container: {
+    padding: 16,
+  },
+});
