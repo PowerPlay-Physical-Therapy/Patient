a1c3538ca92e7c8e72b50fed67f692f02fa8fcec
21tnguyen <21tnguyen10@gmail.com>
2025-05-05 01:36:13 -0700

    Update _layout.tsx

--- app/(tabs)/progress/_layout.tsx
+++ app/(tabs)/progress/_layout.tsx
@@ -1,10 +1,25 @@
 
 import { Stack } from "expo-router";
+import { AppColors } from "@/constants/Colors";
 
 export default function ProfileLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="completedExercises" options={{headerStyle: {
+                backgroundColor: AppColors.OffWhite,
+                },
+                headerBackTitle: 'Progress',
+                headerShown: true,
+                title: "Completion Exercise History",
+            }}/>
+            <Stack.Screen name="completedRoutines" options={{headerStyle: {
+                backgroundColor: AppColors.OffWhite,
+                },
+                headerBackTitle: 'Progress',
+                headerShown: true,
+                title: "Completion History",
+            }}/>
+            {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
         </Stack>
     );
 }
